ebe40e26ff9728f7c49db9c8269cbb32b69d45cb
Vitaliy <vspolets@gmail.com>
2026-01-04 23:24:08 +0200

    edit files js

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,10 @@
-function getShippingMessage(country, price, deliveryFee) { 
-
-
+function getShippingMessage(country, price, deliveryFee) {
+    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
+
+getShippingMessage("Australia", 120, 50);
+getShippingMessage("Germany", 80, 20);
+getShippingMessage("Sweden", 100, 20)
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,18 @@
-function getElementWidth(content, padding, border) { 
-
-
+function getElementWidth(content, padding, border) {
+    
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  
 } 
+
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
